20223562a7706b4b28418bafe3526290c1747a1d
Lars <lduinker04@gmail.com>
2025-10-08 12:32:46 +0200

    Oppdaterte oversettelse scriptet og la til noen oversettelser

--- index.js
+++ index.js
@@ -4,16 +4,45 @@ const languages = data
 
 let current_language = localStorage.getItem("language") || "no"
 
-const england_button = document.getElementById("en")
-const norway_button = document.getElementById("no")
+var highlight_translated_elements = localStorage.getItem("highlight") || false
+
+/**
+ * @param {(element:HTMLElement,key:String,text:String)=>void} callback 
+ * @param {String} ln 
+ */
+function forAllTranslatables(callback,ln){
+    for(const [key,text] of Object.entries(languages[ln || current_language])){
+        document.querySelectorAll(`[translation_key=${key}]`).forEach(element=>{
+            callback(element,key,text)
+        })
+    }
+}
+
+function toggleHighlight(){   
+    highlight_translated_elements = !highlight_translated_elements  
+    localStorage.setItem("highlight",highlight_translated_elements) 
+    highlight(highlight_translated_elements)
+}
+
+function highlight(state){
+    forAllTranslatables(el=>el.style.color = state ? "red" : "")
+}
+
 
 function loadLanguage(ln){
     localStorage.setItem("language",ln)
-    for(const [id,text] of Object.entries(languages[ln])){
-        document.getElementById(id).innerText = text
+    forAllTranslatables((el,_,text)=>{
+        el.innerText = text
+        highlight_translated_elements && (el.style.color = "red")
+    },ln)
+}
+function activateButtons(){
+    const england_button = document.getElementById("en")
+    const norway_button = document.getElementById("no")
+    if(england_button && norway_button){
+        england_button.onclick = () => loadLanguage("en")
+        norway_button.onclick = () => loadLanguage("no")
     }
 }
-england_button.onclick = () => loadLanguage("en")
-norway_button.onclick = () => loadLanguage("no")
-
-loadLanguage(current_language)
+activateButtons()
+highlight(highlight_translated_elements)
--- languages.js
+++ languages.js
@@ -1,21 +1,27 @@
 
 data = {
     "en":{
-        test:"Hi this is a test text",
-        foo:"foo",
-        bar:"Billijard",
         language:"Language",
         en:"English",
         no:"Norwegian",
-        about_us:"About us"
+        about_us:"About us",
+        home:"Home",
+        clothes:"Clothes",
+        products:"Products",
+        sponsors:"Sponsors",
+        pictures:"Pictures",
+        puddles:"Puddles"
     },
     "no":{
-        test:"Hei dette er en test tekst",
-        foo:"doo",
-        bar:"Millijon",
         language:"Språk",
         en:"Engelsk",
         no:"Norsk",
-        about_us:"Om oss"
+        about_us:"Om oss",
+        home:"Hjem",
+        clothes:"Klær",
+        products:"Produkter",
+        sponsors:"Sponsorer",
+        pictures:"Bilder",
+        puddles:"Sølepytter"
     }
 }
